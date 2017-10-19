ac64e256c2ae4cb181c13ea9d3d5e90e67ff9c2b
Miguel Gomes <miguel.teixeira.gomes@gmail.com>
2017-10-19 23:10:41 +0100

    Removed the boost dependency.

--- dimensional_analysis.h
+++ dimensional_analysis.h
@@ -1,54 +1,52 @@
 #pragma once
 
-#include <boost\cstdfloat.hpp>
-#include <boost\mpl\set.hpp>
-#include <boost\mpl\assert.hpp>
-#include <boost\multiprecision\cpp_dec_float.hpp>
-#include <boost\static_assert.hpp>
-#include <boost\variant.hpp>
 #include "dimensions.h"
 
 //#define SKIP_ALL
-#define INT8 int8
+#define INT8  int8
 #define INT16 int16
 #define INT32 int32
 #define INT64 int64
-#define UINT8 uint8
+#define UINT8  uint8
 #define UINT16 uint16
 #define UINT32 uint32
 #define UINT64 uint64
 #define FLOAT32 float32
 #define FLOAT64 float64
 
+#define FLOAT32_T float
+#define FLOAT64_T double
+
 
 #ifndef SKIP_ALL
+	
+	#define IS_NUMERIC_TYPE(TYPE) (std::is_same<TYPE, std::int8_t>::value    | \
+								   std::is_same<TYPE, std::int16_t>::value   | \
+								   std::is_same<TYPE, std::int32_t>::value   | \
+								   std::is_same<TYPE, std::int64_t>::value   | \
+								   std::is_same<TYPE, std::uint8_t>::value   | \
+								   std::is_same<TYPE, std::uint16_t>::value  | \
+								   std::is_same<TYPE, std::uint32_t>::value  | \
+								   std::is_same<TYPE, std::uint64_t>::value  | \
+								   std::is_same<TYPE, FLOAT32_T>::value | \
+								   std::is_same<TYPE, FLOAT64_T>::value)
 
-	typedef boost::variant<std::int8_t, 
-						   std::int16_t, 
-						   std::int32_t, 
-						   std::int64_t, 
-						   std::uint8_t, 
-						   std::uint16_t, 
-						   std::uint32_t, 
-						   std::uint64_t, 
-						   boost::float32_t, 
-						   boost::float64_t> AllowedPrimitiveTypes;
+	#define IS_INTEGER_TYPE(TYPE) (std::is_same<TYPE, std::int8_t>::value   | \
+								   std::is_same<TYPE, std::int16_t>::value  | \
+								   std::is_same<TYPE, std::int32_t>::value  | \
+								   std::is_same<TYPE, std::int64_t>::value  | \
+								   std::is_same<TYPE, std::uint8_t>::value  | \
+								   std::is_same<TYPE, std::uint16_t>::value | \
+								   std::is_same<TYPE, std::uint32_t>::value | \
+								   std::is_same<TYPE, std::uint64_t>::value)
 
-	typedef boost::variant<std::int8_t, 
-						   std::int16_t, 
-						   std::int32_t, 
-						   std::int64_t, 
-						   std::uint8_t, 
-						   std::uint16_t, 
-						   std::uint32_t, 
-						   std::uint64_t> AllowedPrimitiveIntegers;
 
 	template<typename NumT> struct NumericValue {
 		static const NumT value;
 	};
 
 	template<typename NumT, typename Dims = Adimensional> class PrimitiveType {
-		static_assert(boost::mpl::contains<AllowedPrimitiveTypes::types, NumT>::type::value, "Only primitive numeric types are allowed.");
+		static_assert(IS_NUMERIC_TYPE(NumT), "Only primitive numeric types are allowed.");
 
 		public:
 			NumT value;
@@ -112,21 +110,6 @@
 
 			SAME_UNITS_COMPOUND_ASSIGNMENT(+=, Unmatched_dimensions_in_compound_addition_between)
 			SAME_UNITS_COMPOUND_ASSIGNMENT(-=, Unmatched_dimensions_in_compound_addition_between)
-
-
-			template<typename NumT_alt = NumT, typename Dims_alt = Dims>
-				/*inline typename std::enable_if<boost::mpl::contains<AllowedPrimitiveIntegers::types, NumT_alt>::type::value,*/ PrimitiveType<decltype(~NumericType<NumT_alt>::value), Dims_alt> /*>::type*/
-					operator~() {
-						return PrimitiveType<decltype(~NumericType<NumT_alt>::value), Dims_alt>(~(this->value));
-			}
-			
-			/*struct Bitwise_negation_is_only_available_for_adimensional_integers;
-
-			template<typename NumT_alt = NumT, typename Dims_alt = Dims>
-				inline typename std::enable_if<!boost::mpl::contains<AllowedPrimitiveIntegers::types, NumT_alt>::type::value, Bitwise_negation_is_only_available_for_adimensional_integers_not>::type
-					operator~() {
-						return PrimitiveType<decltype(~PrimitiveType<NumT_alt, Dims_alt>::value), Dims_alt>(~(this->value));
-			}*/
 	};
 
 	#define SAME_UNITS_OPERATOR(OPERATOR, ERROR_MESSAGE)\
@@ -192,46 +175,50 @@
 	BITWISE_BINARY_OPERATOR(>>, Bitwise_right_shift_is_only_available_between_adimensional_numbers_and_not_for)
 
 
-	template<typename Dims> using INT8  = PrimitiveType<std::int8_t , Dims>;
-	template<typename Dims> using INT16 = PrimitiveType<std::int16_t, Dims>;
-	template<typename Dims> using INT32 = PrimitiveType<std::int32_t, Dims>;
-	template<typename Dims> using INT64 = PrimitiveType<std::int64_t, Dims>;
-
-	template<typename Dims> using UINT8  = PrimitiveType<std::uint8_t , Dims>;
-	template<typename Dims> using UINT16 = PrimitiveType<std::uint16_t, Dims>;
-	template<typename Dims> using UINT32 = PrimitiveType<std::uint32_t, Dims>;
-	template<typename Dims> using UINT64 = PrimitiveType<std::uint64_t, Dims>;
-
-	template<typename Dims> using FLOAT32 = PrimitiveType<boost::float32_t, Dims>;
-	template<typename Dims> using FLOAT64 = PrimitiveType<boost::float64_t, Dims>;
+	template<typename NumT> struct Bitwise_negation_is_only_available_for_integers_and_not;
+	Bitwise_negation_is_only_available_for_integers_and_not<FLOAT32_T> operator~(PrimitiveType<FLOAT32_T, Adimensional> x);
+	Bitwise_negation_is_only_available_for_integers_and_not<FLOAT64_T> operator~(PrimitiveType<FLOAT64_T, Adimensional> x);
+	template<typename NumT> PrimitiveType<NumT, Adimensional> operator~(PrimitiveType<NumT, Adimensional> x) {
+		return PrimitiveType<NumT, Adimensional>(~x.value);
+	}
+	template<typename NumT, typename Dims> struct Bitwise_negation_is_only_available_for_adimensional_quantities_and_not;
+	template<typename NumT, typename Dims> Bitwise_negation_is_only_available_for_adimensional_quantities_and_not<NumT, Dims> operator~(PrimitiveType<NumT, Dims> x);
 
 
-	/*using INT8  = INT8<Adimensional>;
-	using INT16 = INT16<Adimensional>;
-	using INT32 = INT32<Adimensional>;
-	using INT64 = INT64<Adimensional>;
+	template<typename NumT_lhs, typename NumT_rhs, typename DimsLhs, typename DimsRhs>
+		inline PrimitiveType<decltype(NumericValue<NumT_lhs>::value % NumericValue<NumT_rhs>::value), DimsLhs>
+			operator%(PrimitiveType<NumT_lhs, DimsLhs> lhs, PrimitiveType<NumT_rhs, DimsRhs> rhs) {
+				return PrimitiveType<decltype(NumericValue<NumT_lhs>::value % NumericValue<NumT_rhs>::value), DimsLhs>(lhs.value % rhs.value);
+	}
 
-	using UINT8  = UINT8<Adimensional>;
-	using UINT16 = UINT16<Adimensional>;
-	using UINT32 = UINT32<Adimensional>;
-	using UINT64 = UINT64<Adimensional>;
 
-	using FLOAT32 = FLOAT32<Adimensional>;
-	using FLOAT64 = FLOAT64<Adimensional>;*/
+	// typedefing the primitive types
+	template<typename Dims = Adimensional> using INT8  = PrimitiveType<std::int8_t , Dims>;
+	template<typename Dims = Adimensional> using INT16 = PrimitiveType<std::int16_t, Dims>;
+	template<typename Dims = Adimensional> using INT32 = PrimitiveType<std::int32_t, Dims>;
+	template<typename Dims = Adimensional> using INT64 = PrimitiveType<std::int64_t, Dims>;
+
+	template<typename Dims = Adimensional> using UINT8  = PrimitiveType<std::uint8_t , Dims>;
+	template<typename Dims = Adimensional> using UINT16 = PrimitiveType<std::uint16_t, Dims>;
+	template<typename Dims = Adimensional> using UINT32 = PrimitiveType<std::uint32_t, Dims>;
+	template<typename Dims = Adimensional> using UINT64 = PrimitiveType<std::uint64_t, Dims>;
+
+	template<typename Dims = Adimensional> using FLOAT32 = PrimitiveType<FLOAT32_T, Dims>;
+	template<typename Dims = Adimensional> using FLOAT64 = PrimitiveType<FLOAT64_T, Dims>;
 
 #else
 
-	template<typename Dims> using INT8  = std::int8_t;
-	template<typename Dims> using INT16 = std::int16_t;
-	template<typename Dims> using INT32 = std::int32_t;
-	template<typename Dims> using INT64 = std::int64_t;
+	template<typename Dims = Adimensional> using INT8  = std::int8_t;
+	template<typename Dims = Adimensional> using INT16 = std::int16_t;
+	template<typename Dims = Adimensional> using INT32 = std::int32_t;
+	template<typename Dims = Adimensional> using INT64 = std::int64_t;
 
-	template<typename Dims> using UINT8  = std::uint8_t;
-	template<typename Dims> using UINT16 = std::uint16_t;
-	template<typename Dims> using UINT32 = std::uint32_t;
-	template<typename Dims> using UINT64 = std::uint64_t;
+	template<typename Dims = Adimensional> using UINT8  = std::uint8_t;
+	template<typename Dims = Adimensional> using UINT16 = std::uint16_t;
+	template<typename Dims = Adimensional> using UINT32 = std::uint32_t;
+	template<typename Dims = Adimensional> using UINT64 = std::uint64_t;
 
-	template<typename Dims> using FLOAT32 = boost::float32_t;
-	template<typename Dims> using FLOAT64 = boost::float64_t;
+	template<typename Dims = Adimensional> using FLOAT32 = FLOAT32_T;
+	template<typename Dims = Adimensional> using FLOAT64 = FLOAT64_T;
 
 #endif
--- tests.cpp
+++ tests.cpp
@@ -11,87 +11,35 @@ void test1() {
 	std::uint16_t u16;
 	std::uint32_t u32;
 	std::uint64_t u64;
-	boost::float32_t f32;
-	boost::float64_t f64;
+	float f32;
+	double f64;
 	bool b8;
 
-	auto c = i8;
+	//auto c = ~i8;
 
 	int32<Time> a = 5;
-	int32<Adimensional> b = 2;
+	int32<Time> b = 2;
+	int64<> c = 7;
+	int64<> d = 11;
 
-	//std::cout << (~b) << '\n';
-	/*std::cout << a + b << "\n";
-	std::cout << a - b << "\n";
-	std::cout << a * b << "\n";
-	std::cout << a / b << "\n";
-	std::cout << (a == b) << "\n";
-	std::cout << (a != b) << "\n";
-	std::cout << (a > b) << "\n";
-	std::cout << (a < b) << "\n";
-	std::cout << (a >= b) << "\n";
-	std::cout << (a <= b) << "\n";*/
+	std::cout << "+:  " << a + b << "\n";
+	std::cout << "-:  " << a - b << "\n";
+	std::cout << "*:  " << a * b << "\n";
+	std::cout << "/:  " << a / b << "\n";
+	std::cout << "%:  " << a % b << "\n";
+	std::cout << "==: " << (a == b) << "\n";
+	std::cout << "!=: " << (a != b) << "\n";
+	std::cout << ">:  " << (a > b) << "\n";
+	std::cout << "<:  " << (a < b) << "\n";
+	std::cout << ">=: " << (a >= b) << "\n";
+	std::cout << "<=: " << (a <= b) << "\n";
+	std::cout << "~:  " << (~c) << '\n';
+	std::cout << "|:  " << (c | d) << "\n";
+	std::cout << "&:  " << (c & d) << "\n";
+	std::cout << "^:  " << (c ^ d) << "\n";
+	std::cout << "<<: " << (c << d) << "\n";
+	std::cout << ">>: " << (c >> d) << "\n";
 }
-
-/*#include <iostream>
-#include <stdint.h>
-
-//#define DEBUG
-template<int lenght, int time, int mass, int charge, int temperature, int amount, int intensity> 
-struct Dimensions {
-	static const int64_t LENGHT = lenght;
-	static const int64_t TIME = time;
-	static const int64_t MASS = mass;
-	static const int64_t CHARGE = charge;
-	static const int64_t TEMPERATURE = temperature;
-	static const int64_t AMOUNT = amount;
-	static const int64_t INTENSITY = intensity;
-};
-
-typedef Dimensions<  0, 0, 0, 0, 0, 0, 0 > Adimensional;
-typedef Dimensions<  1, 0, 0, 0, 0, 0, 0 > Length;
-typedef Dimensions<  0, 1, 0, 0, 0, 0, 0 > Time;
-
-#ifdef DEBUG
-	template<typename Dims> class Int32 {
-	private:
-		int32_t m_value;
-
-	public:
-		inline Int32() : m_value(0) {}
-
-		inline Int32(int32_t value) : m_value(value) {}
-
-		inline int32_t value() {
-			return m_value;
-		}
-	};
-
-	template<typename Dims> 
-	Int32<Dims> inline operator+(Int32<Dims> &lhs, Int32<Dims> &rhs) {
-		return Int32<Dims>(lhs.value() + rhs.value());
-	}
-
-	template<typename Dims> inline std::ostream &operator<<(std::ostream &os, Int32<Dims> &m) {
-			return os << m.value();
-	}
-
-	struct Unmatched_dimensions_between_operands;
-
-	template<typename DimsLhs, typename DimsRhs>
-	Unmatched_dimensions_between_operands inline operator+(Int32<DimsLhs> &lhs, Int32<DimsRhs> &rhs);
-#else
-	template<typename Dims> using Int32 = std::int32_t;
-#endif
-
-int main(int argc, char* argv[]) {
-	Int32<Time> a = 2;
-	Int32<Time> b = 5;
-
-	std::cout << a + b << "\n";
-	return 0;
-}*/
-
 
 
 int main(int argc, char* argv[]) {
